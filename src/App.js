7c9bcde0ecdaf4c6f489736eac425141af4f7ac9
user-1289 <armaanzeyad@gmail.com>
2023-05-24 20:54:00 +0530

    changed text in header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <h2>Welcome to <br/>Deverse.space</h2>
-      <h3>These are our contributions</h3>
+      <h3>These are our services</h3>
       <div className='sign'>
       <span className='sign-txt'>Sign up</span>
         <span className='log-txt'>Log in</span>
