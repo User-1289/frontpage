4f81673f9f1f417e1fafccaa993e14a651e8b770
user-1289 <armaanzeyad@gmail.com>
2023-05-24 21:17:42 +0530

    added font from google

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Container from './Container';
 function App() {
   return (
     <div className="App">
-      <h2>Welcome to <br/>Deverse.space</h2>
+      <h2 className='welcome-txt'>Welcome to <br/><span className='name-txt'>Deverse.space</span></h2>
       <h3>These are our services</h3>
       <div className='sign'>
       <span className='sign-txt'>Sign up</span>
