e8c329c905cf352488decb285068840c88db4c54
user-1289 <armaanzeyad@gmail.com>
2023-05-28 12:39:21 +0530

    removed some websites and made responsive added footer

--- src/Container.js
+++ src/Container.js
@@ -10,31 +10,26 @@ const Container = () => {
         <span className='sub-header'>Read news and <br/>search on the go</span>
       </div>
     </div>
-    <div className="container-2">
-      <div style={{color:"white", position:'absolute'}} className="content">
-        <span className='header-txt'>Facts</span><br/>
-        <span className='sub-header'>Discover surprising,<br/>mindblowing facts</span>
-      </div>
-    </div>
-    <div onClick={ () => {window.open("https://todo.deverse.space", "_blank")}} className="container-3">
+
+    <div onClick={ () => {window.open("https://todo.deverse.space", "_blank")}} className="container-2">
       <div  className="content">
         <span className='header-txt'>To do</span><br/>
         <span className='sub-header'>Organize your daily<br/>day tasks</span>
       </div>
     </div>
-    <div onClick={ () => {window.open("https://mirror-lavish-bagel.glitch.me/", "_blank")}} className="container-4">
+    <div onClick={ () => {window.open("https://mirror-lavish-bagel.glitch.me/", "_blank")}} className="container-3">
       <div style={{color:"white"}}  className="content">
         <span className='header-txt'>Weather</span><br/>
         <span className='sub-header'>Check the climate of<br/> your surroundings</span>
       </div>
     </div>
-    <div onClick={ () => {window.open("https://quiz.deverse.space","_blank")}} className="container-5">
+    <div onClick={ () => {window.open("https://quiz.deverse.space","_blank")}} className="container-4">
       <div style={{color:"white"}}  className="content">
         <span className='header-txt'>Quiz</span><br/>
         <span className='sub-header'>Test your knowledge<br/>and have some fun</span>
       </div>
     </div>
-
+<Footer/>
     </>
   );
 };
--- src/Footer.js
+++ src/Footer.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import './footer.css'
-import facebookimage from './facebook.png'
+import React from 'react';
+import './footer.css';
+import twitter from './twitter.png';
+import facebook from './facebook.png'
 export default function Footer() {
   return (
     <footer className='footer'>
-        <div>
-<span className='text-footer'>About</span>
-<span className='text-footer'>Privacy</span>
-<span className='text-footer'>Terms</span>
-<span className='text-footer'>Contact</span>
-<img src={facebookimage}/>
-        </div>
+      <div>
+        <span className='text-footer'>About</span>
+        <span className='text-footer'>Privacy</span>
+        <span className='text-footer'>Terms</span>
+        <span className='text-footer'>Contact</span>
+        <span className='text-footer'><img src={twitter} alt="Twitter" /></span>
+
+      </div>
     </footer>
-  )
+  );
 }
